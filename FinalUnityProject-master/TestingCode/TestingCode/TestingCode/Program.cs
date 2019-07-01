9632900e6c7dd671b52d702e671b71220404e17e
akira2632 <akira2632@gmail.com>
2019-07-01 15:32:39 +0800

    重新設計RandomMap物件結構

---
+++ FinalUnityProject-master/TestingCode/TestingCode/TestingCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingCode
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RandomMapV_2 test = new RandomMapV_2();
+
+            test.Run();
+        }
+    }
+}
