9f86541c7f5e101e8623943325c929a55b71e5c4
akira2632 <akira2632@gmail.com>
2019-07-08 01:35:59 +0800

    增加測試用場景 物件 TileMap等等 正式導入地圖生成程式碼

---
+++ FinalUnityProject/FinalUnityProject/Assets/Script/IBlockBuilder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MapGenerater
+{
+    //區塊建造者介面
+    interface IBlockBuilder
+    {
+        bool HasCompleteBlock(Coordinate coordinate);
+        bool HasBoundary(Coordinate coordinate, Direction direction);
+
+        void ConectBoundary(Coordinate coordinate, Direction direction);
+
+        void MakeWall(Coordinate coordinate, Direction direction);
+        void MakePath(Coordinate coordinate, Direction direction);
+        void MakeBlock(Coordinate coordinate);
+    }
+}
---
+++ FinalUnityProject/FinalUnityProject/Assets/TestResurece/MoveItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveItem : MonoBehaviour
+{
+    [Header("ItemSpeed")]
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.up * Speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.down * Speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+    }
+}
