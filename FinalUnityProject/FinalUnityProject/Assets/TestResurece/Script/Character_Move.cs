ac9c35afd736e2225d3e44939c66dca70471d7ea
akira2632 <akira2632@gmail.com>
2019-07-16 16:55:34 +0800

    角色移動、待機、攻擊測試完成 Animator好複雜 (死

--- FinalUnityProject/FinalUnityProject/Assets/TestResurece/Script/Character_Move.cs
+++ FinalUnityProject/FinalUnityProject/Assets/TestResurece/Script/Character_Move.cs
@@ -3,10 +3,8 @@ using UnityEngine.Animations;
 
 public class Character_Move : MonoBehaviour
 {
-    [SerializeField]
-    Animator animator;
-    [SerializeField]
-    Rigidbody2D rigidbody2d;
+    public Animator animator;
+    public Rigidbody2D rigidbody2d;
 
     Vector2 position;
     int x, y, action;
@@ -16,8 +14,8 @@ public class Character_Move : MonoBehaviour
     void Start()
     {
         Speed = 5.0f;
-        x = 0;
-        y = 0;
+        x = -1;
+        y = 1;
         action = 0;
     }
 
@@ -26,51 +24,64 @@ public class Character_Move : MonoBehaviour
     {
         position = rigidbody2d.position;
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if(action != 2)
         {
-            x = 0;
-            action = 1;
-            position += Vector2.left * Speed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            x = 2;
-            action = 1;
-            position += Vector2.right * Speed * Time.deltaTime;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            action = 0;
-            x = 1;
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                x = -1;
+                action = 1;
+                position += Vector2.left * Speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                x = 1;
+                action = 1;
+                position += Vector2.right * Speed * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                y = 1;
+                action = 1;
+                position += Vector2.up * 0.5f * Speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                y = -1;
+                action = 1;
+                position += Vector2.down * 0.5f * Speed * Time.deltaTime;
+            }
+            
+            if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                action = 0;
+                x = 0;
+            }
+
+            if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                action = 0;
+                y = 0;
+            }
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            y = 0;
-            action = 1;
-            position += Vector2.up * 0.5f * Speed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            y = 6;
-            action = 1;
-            position += Vector2.down * 0.5f * Speed * Time.deltaTime;
-        }
-        else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            action = 0;
-            y = 3;
-        }
+        if (Input.GetKeyDown(KeyCode.G))
+            action = 2;
 
         rigidbody2d.MovePosition(position);
-
         
-        if (action == 0)
-            animator.SetInteger("Action", 0);
-        else if (action == 1)
-            animator.SetInteger("Action", 1);
+        animator.SetInteger("Action", action);
+        
+        if(! (x==0 && y == 0))
+        {
+            animator.SetFloat("X", x);
+            animator.SetFloat("Y", y);
+        }
+    }
 
-        if ((x + y) != 4)
-            animator.SetInteger("Direction", x + y);
+    public void AttackEnd()
+    {
+        action = 0;
+        Debug.Log("has called");
     }
 }
