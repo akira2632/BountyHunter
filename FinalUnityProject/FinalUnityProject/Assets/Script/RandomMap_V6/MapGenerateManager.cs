3994ee3839a785074d364cd6f5f1fd8946750f0a
akira2632 <akira2632@gmail.com>
2019-08-12 12:14:21 +0800

    匯入隨機地圖系統

---
+++ FinalUnityProject/FinalUnityProject/Assets/Script/RandomMap_V6/MapGenerateManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace RandomMap_V6
+{
+    public class MapGenerateManager : MonoBehaviour
+    {
+        [Header("地圖深度"), Range(1, 100), Tooltip("自起點至最深處的區塊數量")]
+        public int MapScale;
+        [Header("地圖種子"), Tooltip("0由電腦自動產生種子")]
+        public int seed;
+
+        public Grid grid;
+
+        public MiniMapSetting MiniMapSetting = new MiniMapSetting();
+        public GameMapSetting GameMapSetting = new GameMapSetting();
+        private GeneraterFactry generaterFactry;
+
+        #region 生成頻率管理
+        IGenerater generater;
+        bool hasInitail;
+        int ticks;
+        public float StartTime, ScaleStartTime;
+
+
+        private void Start()
+        {
+            SetSeed();
+            generaterFactry = new GeneraterFactry(MiniMapSetting, GameMapSetting, this);
+
+            //初始化狀態機
+            hasInitail = false;
+            generater = new AreaGenerateInitail(MapScale, this);
+
+            //Debug.Log("MapGenerateManager Start");
+        }
+
+
+        private void Update()
+        {
+            ticks = 0;
+
+            while (ticks < 100)
+            {
+                GeneraterUpdate();
+                //Debug.Log("FinalTicks : " + ticks);
+            }
+        }
+
+        private void GeneraterUpdate()
+        {
+            if (!hasInitail)
+            {
+                hasInitail = true;
+                generater.Initail();
+            }
+            else
+                generater.Update();
+        }
+
+        public void SetNextGenerater(IGenerater nextGenerter)
+        {
+            generater.End();
+            generater = nextGenerter;
+            hasInitail = false;
+        }
+
+        public void AddTicks(int ticks = 1)
+        {
+            this.ticks += ticks;
+        }
+        #endregion
+
+        private void SetSeed()
+        {
+            if (seed != 0)
+                Random.InitState(seed);
+            else
+                seed = Random.seed;
+        }
+
+        internal GeneraterFactry GetGeneraterFactry() => generaterFactry;
+    }
+}
