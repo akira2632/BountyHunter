a86fc68082fae5a04d68745da3679cfac255a72c
akira2632 <akira2632@gmail.com>
2020-01-06 18:30:45 +0800

    發現並解決單次攻擊產生複數傷害的問題

--- FinalUnityProject/FinalUnityProject/Assets/Script/CharacterSystem_V4/Utility/AttackColliders.cs
+++ FinalUnityProject/FinalUnityProject/Assets/Script/CharacterSystem_V4/Utility/AttackColliders.cs
@@ -6,13 +6,31 @@ namespace CharacterSystem_V4
     {
         public Wound MyDamage;
         public string TargetTag;
-        
+
+        private bool hasHitTarget;
+
+        private void Start()
+        {
+            hasHitTarget = false;
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.gameObject.tag == TargetTag)
+            if (!hasHitTarget && collision.gameObject.tag == TargetTag)
             {
+                Debug.Log($"Target Enter : {TargetTag}");
+                hasHitTarget = true;
                 MyDamage.KnockBackFrom = gameObject.gameObject.transform.position;
                 collision.gameObject.GetComponentInParent<ICharacterActionManager>().OnHit(MyDamage);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == TargetTag)
+            {
+                Debug.Log($"Target Exit: {TargetTag}");
+                hasHitTarget = false;
             }
         }
     }
