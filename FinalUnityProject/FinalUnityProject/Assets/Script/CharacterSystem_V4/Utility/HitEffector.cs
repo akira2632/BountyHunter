37cb01836a1710966f7a697f6427bef0a386d562
akira2632 <akira2632@gmail.com>
2020-01-21 00:12:25 +0800

    傷害數字特效完成 簡單整理程式碼

--- FinalUnityProject/FinalUnityProject/Assets/Script/CharacterSystem_V4/Utility/HitEffector.cs
+++ FinalUnityProject/FinalUnityProject/Assets/Script/CharacterSystem_V4/Utility/HitEffector.cs
@@ -6,6 +6,7 @@ using CharacterSystem_V4;
 
 public class HitEffector : MonoBehaviour
 {
+    #region 輔助用結構
     public enum SpriteDirection { Left = -1, None, Right }
 
     [System.Serializable]
@@ -15,29 +16,62 @@ public class HitEffector : MonoBehaviour
         [Tooltip("效果的預設方向")]
         public SpriteDirection Direction;
     }
+    #endregion
 
+    #region 設定
+    #region 測試設定
     [Header("TestSetting")]
     [Min(0)]
     public int test_Damage;
     public bool test_ShowDamage;
     public Vector2 test_HitAt;
-
+    #endregion
+    #region 預製物設定
     [Space(10)]
     [Header("PrafebSetting")]
     public GameObject[] NumberPrafebs;
     public EffectPrafeb[] HitEffectPrafebs, DeffendEffectPrafebs;
     [Tooltip("文字的間距")]
     public float NumberPitch;
-
+    #endregion
+    #region 傷害數字效果設定
     [Space(10)]
-    [Header("EffectSetting")]
-    public float HitEffectTime; 
+    [Header("NumberEffectSetting")]
     public float NumberEffectTime;
-    public float EndHight, EndVertical;
-    public bool EnableHorizontal = true;
+    public float NumberEndHight, NumberEndVertical;
+    public Color NumberEndColor;
+    public Ease NumberColorEase = Ease.InSine, 
+        NumberHorizontalEase = Ease.InOutCubic,
+        NumberVerticalEase = Ease.OutBack;
+    public bool EnableHorizontal_Number = true;
+    #endregion
+    #region 擊中特效設定
+    [Space(10)]
+    [Header("HitEffectSetting")]
+    public float HitEffectTime;
+    public float HitEndHight, HitEndVertical;
+    public Color HitEndColor;
+    public Ease HitColorEase = Ease.InSine,
+        HitHorizontalEase = Ease.InOutCubic,
+        HitVerticalEase = Ease.OutBack;
+    public bool EnableHorizontal_Hit = true;
+    #endregion
+    #endregion
 
+    #region Inspector功能
     [ContextMenu("PlayHitEffect")]
     private void PlayHitEffect()
+    {
+        PlayHitEffect(new DamageData() 
+        { 
+            Damage = test_Damage,
+            HitAt = test_HitAt,
+            HitFrom = transform.position
+        }, test_ShowDamage);
+    }
+
+    [ContextMenu("PlayDeffendEffect")]
+    private void PlayDeffendEffect()
     {
         PlayDeffendEffect(new DamageData() 
         { 
@@ -46,6 +80,7 @@ public class HitEffector : MonoBehaviour
             HitFrom = transform.position
         }, test_ShowDamage);
     }
+    #endregion
 
     public void PlayHitEffect(DamageData damage, bool showNumber = true)
     {
@@ -67,20 +102,23 @@ public class HitEffector : MonoBehaviour
         temp.transform.position = damage.HitAt;
         var renderers = AssambellyNumber(damage.Damage, temp);
 
-        var endColor = new Color(1, 1, 1, 0);
         foreach (SpriteRenderer renderer in renderers)
         {
-            renderer.DOColor(endColor, NumberEffectTime).SetEase(Ease.InExpo);
+            renderer.DOColor(NumberEndColor, NumberEffectTime).SetEase(NumberColorEase);
         }
         
         temp.transform.DOBlendableMoveBy(
-            transform.position + new Vector3(0, EndHight), NumberEffectTime)
-            .SetEase(Ease.OutExpo)
+            transform.position + new Vector3(0, NumberEndHight), NumberEffectTime)
+            .SetEase(NumberVerticalEase)
             .onComplete += () => Destroy(temp);
 
-        if(EnableHorizontal)
+        if(EnableHorizontal_Number)
         {
-            //temp.transform.DOBlendableMoveBy();
+            var horizontalOffset = new Vector3(damage.HitFrom.x - damage.HitAt.x, 0).normalized * NumberEndVertical;
+            //Debug.Log(horizontalOffset);
+            temp.transform.DOBlendableMoveBy(
+                transform.position + horizontalOffset, NumberEffectTime)
+                .SetEase(NumberHorizontalEase);
         }
 
         SpriteRenderer[] AssambellyNumber(int value, GameObject parent)
