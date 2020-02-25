c9a95b6857552ba0eaffbdfb74dc02efbc0be4db
akira2632 <akira2632@gmail.com>
2020-02-26 00:25:00 +0800

    新增TutorialBase類別 將Tutorial_Move的動畫設定提取至TutorialBase

--- FinalUnityProject/FinalUnityProject/Assets/Script/UI/Tutorial/TutorialBase.cs
+++ FinalUnityProject/FinalUnityProject/Assets/Script/UI/Tutorial/TutorialBase.cs
@@ -1,8 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Character;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class TutorialBase : MonoBehaviour
 {
+    public GameObject NextTutorial;
+    [Min(0)]
+    public float MoveTime = 3;
+    [Header("AnimateSetting")]
+    public float StartTime;
+    [Range(0, 1)]
+    public float HightLightTimeOffset;
+    public float EndTime;
+    [ColorUsage(true)]
+    public Color StartColor, HightLightColor, EndColor;
 
+    protected WarriorEventsManager warriorEventsManager;
+    protected Image tutorialImage;
+    protected bool tutorialReady;
+
+    private void Start()
+    {
+        tutorialReady = false;
+        tutorialImage = GetComponent<Image>();
+        warriorEventsManager = GetComponentInParent<WarriorEventsManager>();
+
+        ChildrenStart();
+        StartAnimate();
+    }
+
+    protected virtual void ChildrenStart() { }
+
+    protected void EndAnimate()
+    {
+        tutorialImage.DOColor(EndColor, EndTime).onComplete += () =>
+        {
+            NextTutorial?.SetActive(true);
+            gameObject.SetActive(false);
+        };
+    }
+
+    protected void StartAnimate()
+    {
+        DOTween.Sequence()
+            .Append(tutorialImage.DOColor(HightLightColor, StartTime * HightLightTimeOffset).ChangeStartValue(StartColor))
+            .Append(tutorialImage.DOColor(Color.white, StartTime * (1 - HightLightTimeOffset)))
+            .onComplete += () => tutorialReady = true;
+    }
 }
